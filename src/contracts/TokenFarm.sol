1c8e3cbd14d3a9fc8eb1e1f279c542e0442dd1f8
Akinola Oloyede Solamipe <akinolasolamipe@gmail.com>
2022-05-09 17:14:26 +0100

    Implementation of staking function

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -9,8 +9,35 @@ contract TokenFarm {
     DappToken public dappToken;
     DaiToken public daiToken;
 
+    address[] public stakers;
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
     }
+
+    // Staking tokens(Deposit)
+    function stakeTokens(uint256 _amount) public {
+        //Transfer Mock DAI Token to this contract for staking
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        //update staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        //add users to stakers array *only* if they haven't staked already
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        //update staking status
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    // Unstaking tokens(Withdraw)
+
+    //Issuing tokens
 }
